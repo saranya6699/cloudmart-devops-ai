73f819b52e90906f90bef8e270d0d60810815dc9
EC2 Default User <ec2-user@ip-172-31-25-147.us-west-2.compute.internal>
2025-02-15 20:24:38 +0000

    update

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -94,7 +94,7 @@ const CloudMartMainPage = () => {
           <div className="relative">
             <input
               type="text"
-              placeholder="Search products..."
+              placeholder="Search products....."
               className="pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               value={searchTerm}
               onChange={handleSearch}
